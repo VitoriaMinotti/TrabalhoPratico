d0622311668d4dc6f7e64b18d2996fa5aa42ee32
VitoriaMinotti <vitorialminotti@hotmail.com>
2022-10-14 12:23:34 -0300

    trabalho pratico finalizado

---
+++ src/app/selecao/selecao.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: "app-selecao",
+  templateUrl: "./selecao.component.html",
+  styleUrls: ["./selecao.component.css"],
+})
+export class SelecaoComponent {
+  @Input()
+  titulo: string = "";
+
+  @Input()
+  opcoes: string[] = [];
+
+  @Input()
+  escolhaAte: number = 0;
+
+  opcoesSelecionadas: string[] = [];
+
+  marcaOuDesmarcaOpcao(opcao: string) {
+    if (this.escolhaAte === 1) {
+      this.opcoesSelecionadas = [opcao];
+    } else {
+      const idx = this.opcoesSelecionadas.indexOf(opcao);
+      if (idx === -1) {
+        this.opcoesSelecionadas.push(opcao);
+      } else {
+        this.opcoesSelecionadas.splice(idx, 1);
+      }
+    }
+  }
+
+  opcaoSelecionada(opcao: string) {
+    return this.opcoesSelecionadas.indexOf(opcao) !== -1;
+  }
+
+  opcaoDesabilitada(opcao: string): boolean {
+    return (
+      this.escolhaAte !== 1 &&
+      !this.opcaoSelecionada(opcao) &&
+      this.opcoesSelecionadas.length >= this.escolhaAte
+    );
+  }
+}
